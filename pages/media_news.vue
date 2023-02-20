0c222f4821a0c759155d14be5c2d7aec028995e3
mizhon <mizhon@live.cn>
2023-02-20 12:21:40 +0800

    fix: 修改代码

--- pages/media_news.vue
+++ pages/media_news.vue
@@ -1,33 +1,47 @@
 <template>
   <div class="page-content-wrapper">
     <div class="media-news-container">
-      <section class="tab-section">
-        <CommonNavTab :tabs="tabs" :active-tab="activeNewsTabIndex" @get-tab-index="getActiveNewsTabIndex"  />
-      </section>
-      <section class="news-section">
-        <div class="news-list">
-          <div v-for="(newsItem, idx) in newsList" :key="idx" class="card-wrapper">
-            <NewsCard :info="newsItem" />
+      <!-- PC端 -->
+      <div class="pc-only">
+        <section class="tab-section">
+          <CommonNavTab :tabs="tabs" :active-tab="activeNewsTabIndex" @get-tab-index="getActiveNewsTabIndex"  />
+        </section>
+        <section class="news-section">
+          <div class="news-list">
+            <div v-for="(newsItem, idx) in newsList" :key="idx" class="card-wrapper">
+              <NewsCard :info="newsItem" />
+            </div>
           </div>
-        </div>
-        <div class="check-details">
-          {{ checkDetailsText }}
-        </div>
-      </section>
+          <div class="check-details">
+            <div class="detail-btn">
+              <span>
+                {{ checkDetailsText }}
+              </span>
+              <span>
+                <i class="el-icon-arrow-down"></i>
+              </span>
+            </div>
+          </div>
+        </section>
+      </div>
+      <!-- 移动端 -->
+      <div class="mobile-only">
+        <!-- TODO: 添加代码 -->
+      </div>
     </div>
-    <!-- <CommonFooter /> -->
+    <AppFooter :position="position" />
   </div>
 </template>
 <script>
 import CommonNavTab from '@/components/CommonNavTab.vue'
-// import CommonFooter from '@/components/CommonFooter.vue'
+import AppFooter from '@/components/common/AppFooter.vue'
 import NewsCard from '@/components/news/NewsCard.vue'
 
 export default {
   name: 'MediaNewsPage',
   components: {
     CommonNavTab,
-    // CommonFooter,
+    AppFooter,
     NewsCard
   },
   layout: 'normal',
@@ -59,35 +73,66 @@ export default {
           summary: `此处添加说明性文字此处添加说明性文字此处添加说明性文字`
         }
       ],
-      checkDetailsText: '查看详情'
+      checkDetailsText: '查看详情',
+      position: 'fixed',
+      activeTab: 'first'
     }
-  },
-  created() {
-
   },
   methods: {
     getActiveNewsTabIndex(index) {
       this.activeNewsTabIndex = index;
+    },
+    handleTabClick(tab) {
+      // eslint-disable-next-line no-console
+      console.log('click tab: --->', tab)
     }
   }
 }
 </script>
 <style lang="scss" scoped>
-.media-news-container {
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  padding-top: 60px;
-  .tab-section {
-    height: 100vh;
-  }
-  .news-section {
-    width: 50%;
-    height: 100vh;
-    .news-list {
+// PC端样式
+// @media only screen and ()
+
+@media only screen and (min-width: 769px) {
+  .media-news-container {
+    .pc-only {
       display: flex;
-      margin: 0 30px 30px 30px;
+      justify-content: center;
+      align-items: flex-start;
+      padding-top: 60px;
+      .tab-section {
+        height: 100vh;
+      }
+      .news-section {
+        width: 50%;
+        height: 100vh;
+        .news-list {
+          display: flex;
+          margin: 0 30px 30px 30px;
+        }
+        .check-details {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          .detail-btn {
+            margin: 60px 5px 40px 5px;
+            span {
+              padding: 5px;
+              color: #0053a1;
+              cursor: pointer;
+              font-weight: 500;
+            }
+          }
+        }
+      }
     }
+  }
+}
+
+// 移动端样式
+@media only screen and (max-width: 768px) {
+  .pc-only {
+    display: none !important;
   }
 }
 </style>
